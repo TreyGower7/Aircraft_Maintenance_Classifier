0ed39ee261118b1e8d52a2d411a7ac02175af407
Trey Gower <gowertrey@yahoo.com>
2025-07-09 07:13:48 -0500

    base code

--- src/model_training.py
+++ src/model_training.py
@@ -1,8 +1,16 @@
+# pip3 install transformers torch torchvision
 import torch
 import matplotlib.pyplot as plt
 import numpy as np
 import torch.nn as nn
+from transformers import BertModel, BertTokenizer
 
-x = torch.rand(5, 3)
 
-print(x)
+class BERTClassifier(nn.Module):
+    def __init__(self):
+        super(BERTClassifier, self).__init__()
+    
+    def forward():
+        """
+        Define how the data flows in one forward pass
+        """
